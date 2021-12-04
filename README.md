c5bed56895f5a648ec824b9f2e0ca7fa067b79ad
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-12-04 15:07:17 +0530

    Added:welcome message is added

---
+++ README.md
@@ -0,0 +1,1 @@
+EmployeePayRollServiceDay34
---
+++ src/main/java/com/blp/employeepayrollservice/EmployeePayrollDBService.java
@@ -0,0 +1,31 @@
+package com.blp.employeepayrollservice;
+
+import java.sql.*;
+
+public class EmployeePayrollDBService {
+    public static void main(String[] args) throws SQLException {
+        String jdbcUrl="jdbc:mysql://localhost:3306/Payroll_services?useSSL=false";
+        String userName="root";
+        String password="root";
+        Connection con;
+        Statement statement ;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Loaded");
+            con= DriverManager.getConnection(jdbcUrl,userName,password);
+            statement= con.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from employee_payroll");
+            while(resultSet.next()){
+                int  customerId = resultSet.getInt("id");
+                String customerName=resultSet.getString("name");
+                int salary=resultSet.getInt("salary");
+                System.out.print(customerId+" . ");
+                System.out.print(customerName+ " = " );
+                System.out.print(salary+" , ");
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
